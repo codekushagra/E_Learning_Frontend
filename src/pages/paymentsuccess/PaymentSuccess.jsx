84eb2d4c55637db6c73945824848a978836f98a3
KUSHAGRA CHATURVEDI <118090317+codekushagra@users.noreply.github.com>
2024-12-23 06:24:57 +0530

    initial commit

--- src/pages/paymentsuccess/PaymentSuccess.jsx
+++ src/pages/paymentsuccess/PaymentSuccess.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import "./paymentsuccess.css"
-import { Link } from 'react-router-dom'
-import { useParams } from 'react-router-dom'
-import { User } from '../../../../server/models/User'
-import { CourseData } from '../../context/CourseContext'
-import Courses from '../courses/Courses'
+import React from "react";
+import "./paymentsuccess.css";
+import { Link, useParams } from "react-router-dom";
 
-const PaymentSuccess = () => {
-
-    const params = useParams()
+const PaymentSuccess = ({ user }) => {
+  const params = useParams();
   return (
     <div className="payment-success-page">
-        {User && <div className='success-message'>
-            <h2>Payment Successful</h2>
-            <p>Your Subscription has been activated</p>
-            <p> Reference No - {params.id}</p>
-            <Link to={`/${User._id}/dashboard`} className='common-btn'>Go to Dashboard</Link>
-            </div>}
+      {user && (
+        <div className="success-message">
+          <h2>Payment successful</h2>
+          <p>Your course subscription has been activated</p>
+          <p>Reference no - {params.id}</p>
+          <Link to={`/${user._id}/dashboard`} className="common-btn">
+            Go to Dashboard
+          </Link>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default PaymentSuccess
+export default PaymentSuccess;
