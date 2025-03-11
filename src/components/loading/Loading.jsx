50f4bb6e605e163be4915aec811691879335b335
KUSHAGRA CHATURVEDI <118090317+codekushagra@users.noreply.github.com>
2025-03-11 12:25:07 +0530

    Update Loading.jsx

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import "./loading.css"
-
+import React from "react";
+import "./loading.css";
+import loader from "../../../src/loader.gif";
 const Loading = () => {
   return (
-    <div className='loading-page'>
-        <div className='loader'>
-            
-        </div>
+    <div className="loading-page">
+      {/* <div className="loading"></div> */}
+      <img src={loader} alt="Loading..." className="loader-svg" />
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
